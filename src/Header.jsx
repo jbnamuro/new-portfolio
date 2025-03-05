89b84a62e1dbc07a6a2fd9a212efb3d757559429
jbnamuro <jbnamuro12@gmail.com>
2025-03-04 22:16:33 -0500

    up

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
-import { motion } from 'motion/react'
+import React from "react";
+import { motion } from "motion/react";
+import { useState } from "react";
 
 const Header = () => {
+  const [isToggled, setToggled] = useState(false);
   return (
-    <div className='header'>
-        <img src="" alt="logo" />
-        <div className="bars">
-            <div className="bar"></div>
-            <div className="bar"></div>
-            <div className="bar"></div>
-        </div>
+    <div className="header">
+      <img src="" alt="logo" />
+      <div
+        className="bars"
+        onClick={() => {
+          setToggled(!isToggled);
+        }}
+      >
+        <motion.div
+          className="bar"
+          animate={{ rotate: isToggled ? 45 : 0 }}
+        ></motion.div>
+        <motion.div
+          className="bar"
+          animate={{ scale: isToggled ? 0 : 1 }}
+        ></motion.div>
+        <motion.div
+          className="bar"
+          animate={{ rotate: isToggled ? -45 : 0 }}
+        ></motion.div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

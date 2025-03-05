d235da8f60b97ee17d02ce37ef09fd0ec4ae12da
jbnamuro <jbnamuro12@gmail.com>
2025-03-04 21:23:33 -0500

    2025 portfolio site

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { motion } from 'motion/react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <img src="" alt="logo" />
+        <div className="bars">
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Header

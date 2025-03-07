10d9371af1a5aecef74dc30a1af004545a94f4ff
jbnamuro <jbnamuro12@gmail.com>
2025-03-06 20:44:04 -0500

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Menu from "./Menu";
+import LandContent from "./LandContent";
 
 const Home = () => {
   return (
     <div>
       <Menu />
+      <LandContent />
     </div>
   );
 };
---
+++ src/LandContent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const LandContent = () => {
+  return (
+    <div>
+      <h1 id="name">Jabari Namuro</h1>
+      <h2 id="slogan">
+        Making <b>your</b> next website
+      </h2>
+      <div className="work-with">
+        <p>Work with me</p>
+      </div>
+    </div>
+  );
+};
+
+export default LandContent;
